a5b877557c5e1fd7fe5756e351d826a294fec481
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-12-07 15:50:06 -0800

    added crud routes

--- api/accounts/account-router.js
+++ api/accounts/account-router.js
@@ -12,7 +12,44 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-
+    try {
+        const account = req.body;
+        const data = await Accounts.create(account);
+        res.json(data);
+    } catch (err) {
+        res.json(err.message);
+    }
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await Accounts.getById(id);
+        res.json(data);
+    } catch (err) {
+        res.json(err.message);
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const body = req.body;
+        const { id } = req.params;
+        const data = await Accounts.update(id, body);
+        res.json(data);
+    } catch (err) {
+        res.json(err.message);
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await Accounts.delete(id);
+        res.json(data);
+    } catch (err) {
+        res.json(err.message);
+    }
+})
 
 module.exports = router;
--- api/accounts/account.model.js
+++ api/accounts/account.model.js
@@ -1,7 +1,22 @@
 const db = require('../../data/dbConfig');
 
 module.exports = {
-    getAll(){
+    getAll() {
         return db('accounts');
+    },
+    create(account) {
+        return db('accounts').insert(account)
+            .then(([id]) => {
+                return db('accounts').where('id', id).first()
+            })
+    },
+    getById(id){
+        return db('accounts').where('id', id).first();
+    },
+    update(id, account){
+        return db('accounts').where('id', id).update(account)
+    },
+    delete(id){
+        return db('accounts').where('id', id).del();
     }
 }
